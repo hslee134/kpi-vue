555a488d0b6001192fd7709d3d6f300ba6d6e405
sa <hslee134@gmail.com>
2023-12-17 22:37:46 +0900

    upload

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Header/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/vue-header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/NotFound.vue
@@ -0,0 +1,4 @@
+<template>
+    <div>페이지를 찾을 수 없습니다.</div>
+  </template>
+  
